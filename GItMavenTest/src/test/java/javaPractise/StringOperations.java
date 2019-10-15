41ae685e5a03f7970d6bebd0912987565884ecee
shaiksh <shaiksh@US1214156W1.ey.net>
2019-10-14 22:34:07 -0400

    first commit to git

---
+++ GItMavenTest/src/test/java/javaPractise/StringOperations.java
@@ -0,0 +1,12 @@
+package javaPractise;
+
+public class StringOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hi hello delisha");
+		
+	}
+
+}
