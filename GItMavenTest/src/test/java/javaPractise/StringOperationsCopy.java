ca9088bc6b2e03d4694a11401aa348f030ebd3e6
shaiksh <shaiksh@US1214156W1.ey.net>
2019-10-14 22:36:20 -0400

    pushing new file with duplicate code

---
+++ GItMavenTest/src/test/java/javaPractise/StringOperationsCopy.java
@@ -0,0 +1,13 @@
+package javaPractise;
+
+public class StringOperationsCopy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hi hello delisha");
+		System.out.println("my duplicate test");
+		
+	}
+
+}
